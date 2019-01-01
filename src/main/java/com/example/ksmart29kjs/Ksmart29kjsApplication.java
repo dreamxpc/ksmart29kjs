513a37de87c7d15beb720482bf53aa997b57198d
Administrator <Administrator@MSDN-SPECIAL>
2019-01-02 08:45:15 +0900

    ksmart29kjs.cafe24.com

---
+++ src/main/java/com/example/ksmart29kjs/Ksmart29kjsApplication.java
@@ -0,0 +1,14 @@
+package com.example.ksmart29kjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksmart29kjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksmart29kjsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/ksmart29kjs/Ksmart29kjsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.ksmart29kjs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ksmart29kjsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
